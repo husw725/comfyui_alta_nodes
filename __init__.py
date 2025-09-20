0c97be165bd2085adba8641e139122a9d291868e
husw <siwen.hu@melot.cn>
2025-09-20 16:51:24 +0800

    git merge

--- __init__.py
+++ __init__.py
@@ -1,8 +1,10 @@
 from .alta_save_image import SaveImagePlus, NODE_CLASS_MAPPINGS as SAVE_MAPPING
 from .alta_load_images import LoadImagesFromDirectoryPath, NODE_CLASS_MAPPINGS  as LOAD_MAPPING
+from .GptImageMerge import GPTImageMerge, NODE_CLASS_MAPPINGS as GPTIMAGE_MAPPING
 
 
 # 合并成一个字典
 NODE_CLASS_MAPPINGS = {}
 NODE_CLASS_MAPPINGS.update(SAVE_MAPPING)
 NODE_CLASS_MAPPINGS.update(LOAD_MAPPING)
+NODE_CLASS_MAPPINGS.update(GPTIMAGE_MAPPING)
